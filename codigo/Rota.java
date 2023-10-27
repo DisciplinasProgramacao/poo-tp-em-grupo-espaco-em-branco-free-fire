b8d5a98ebcc4f26e2bf044d0233b807fc2da4be3
ArexDiniz <alexgdiniz18@gmail.com>
2023-10-26 21:20:08 -0300

    Criação parcial da Classe Veiculo

--- codigo/Rota.java
+++ codigo/Rota.java
@@ -3,10 +3,20 @@ package codigo;
 import java.util.Date;
 
 public class Rota {
+
     private double quilometragem;
     private Date data;
-    
+
     public String relatorio() {
         return "";
     }
+
+    public double getQuilometragem() {
+        return quilometragem;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
 }
--- codigo/Veiculo.java
+++ codigo/Veiculo.java
@@ -6,35 +6,96 @@ public class Veiculo {
     private String placa;
     private Rota rotas[];
     private int quantRotas;
-    private double tanqueAtual;
-    private double tanqueMax;
+    private Tanque tanqueAtual;
+    private Tanque tanqueMax;
     private double totalReabastecido;
 
     public boolean addRota(Rota rota) {
+        if (rotas[0] != null) {
+            // if verificar se o mes da rotas[0] == ao mês do rota.getData
+            // apenas deixar seguir o processo
+            // else if se a data for diferente
+            /*
+             * for(int i = 0; rotas.length; i++){
+             * rotas[i] = null;
+             * quantRotas = 0;
+             * 
+             * }
+             */
+        }
+
+        if (autonomiaMaxima() >= rota.getQuilometragem()) {
+            rotas[quantRotas] = rota;
+            quantRotas++;
+            percorrerRota(rota);
+            return true;
+        }
+
         return false;
     }
 
     private double autonomiaMaxima() {
-        return 0;
-    } 
+        return tanqueMax.autonomiaMaxima();
+    }
 
-    private double autonomiaAtual () {
-        return 0;
+    private double autonomiaAtual() {
+        return tanqueAtual.autonomiaAtual();
     }
 
     public double abastecer(double litros) {
-        return 0;
-    } 
+        return tanqueAtual.abastecer(litros);
+    }
 
     public double kmNoMes() {
-        return 0;
+        double kmMes = 0;
+        for (Rota rota : rotas)
+            kmMes += rota.getQuilometragem();
+        return kmMes;
     }
 
     public double kmTotal() {
-        return 0;
+        double kmTotal = totalReabastecido * CONSUMO;
+        return kmTotal;
     }
 
     private void percorrerRota(Rota rota) {
+        if (autonomiaAtual() < rota.getQuilometragem()) {
+            abastecer(tanqueMax.autonomiaMaxima() - tanqueAtual.autonomiaAtual());
+            totalReabastecido += (tanqueMax.autonomiaMaxima() - tanqueAtual.autonomiaAtual());
+        }
 
+        tanqueAtual -= (rota.getQuilometragem() / CONSUMO);
+    }
+
+    public int getMAX_ROTAS() {
+        return MAX_ROTAS;
+    }
+
+    public double getCONSUMO() {
+        return CONSUMO;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public Rota[] getRotas() {
+        return rotas;
+    }
+
+    public int getQuantRotas() {
+        return quantRotas;
+    }
+
+    public Tanque getTanqueAtual() {
+        return tanqueAtual;
+    }
+
+    public Tanque getTanqueMax() {
+        return tanqueMax;
+    }
+
+    public double getTotalReabastecido() {
+        return totalReabastecido;
     }
 }
