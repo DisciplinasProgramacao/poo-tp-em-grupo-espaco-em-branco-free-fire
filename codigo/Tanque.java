6f6cf472c068bfc6caf8965a6e0b0806fcfb54cb
Anna-Carla <annacarlateixeira16@gmail.com>
2023-10-30 10:17:13 -0300

    Criação parcial da Classe Tanque

--- codigo/Tanque.java
+++ codigo/Tanque.java
@@ -5,19 +5,29 @@ public class Tanque {
     private double capacidadeMaxima;
     private double capacidadeAtual;
 
-    Tanque () {
-        CONSUMO = 0;
+    Tanque (double consumo, double capacidadeMaxima) {
+        this.CONSUMO = consumo;
+        this.capacidadeMaxima = capacidadeMaxima;
+        this.capacidadeAtual = capacidadeMaxima;
     }
     
     public double abastecer(double litros) {
-        return 0;
+        if (litros > 0) {
+            double capacidadeRestante = capacidadeMaxima - capacidadeAtual;
+            if (litros <= capacidadeRestante) {
+                capacidadeAtual += litros;
+            } else {
+                capacidadeAtual = capacidadeMaxima;
+            }
+        }
+        return capacidadeAtual;
     }
 
     public double autonomiaMaxima() {
-        return 0;
+        return capacidadeMaxima * CONSUMO;
     } 
 
-    public double autonomiaAtual () {
-        return 0;
+    public double autonomiaAtual() {
+        return capacidadeAtual * CONSUMO;
     }
 }
