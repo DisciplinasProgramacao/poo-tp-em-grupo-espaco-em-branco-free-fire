7e879b6435e006a4c4ccb52273789a36317908c6
ArexDiniz <alexgdiniz18@gmail.com>
2023-12-08 20:15:06 -0300

    Troca Do nome da classe ETipoVeiculo

---
+++ codigo/ETipoVeiculo.java
@@ -0,0 +1,31 @@
+package codigo;
+
+public enum ETipoVeiculo {
+    CARRO(50, 10000, 10000),
+    VAN(60, 10000, 12000),
+    FURGAO(80, 10000, 12000),
+    CAMINHAO(250, 20000, 20000);
+
+    private double tamanhoTanque;
+    private int manuPreventiva;
+    private int manuTrocaDePeca;
+
+    private ETipoVeiculo(double tamanhoTanque, int manuPreventiva, int manuTrocaDePeca) {
+        this.tamanhoTanque = tamanhoTanque;
+        this.manuPreventiva = manuPreventiva;
+        this.manuTrocaDePeca = manuTrocaDePeca;
+    }
+
+    public double getTamanhoTanque() {
+        return tamanhoTanque;
+    }
+
+    public int getManuPreventiva() {
+        return manuPreventiva;
+    }
+
+    public int getManuTrocaDePeca() {
+        return manuTrocaDePeca;
+    }
+
+}
